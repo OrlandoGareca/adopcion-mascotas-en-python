58ffd76996f68987070a120a1dc3d71e91358fd4
OrlandoGareca <orlando.dilmar.gareca.pena@gmail.com>
2018-10-08 14:55:35 -0400

    adopcion de mascotas

---
+++ apps/usuario/views.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+
+from ..usuario.forms import RegistroForm
+
+class RegistroUsuario(CreateView):
+    model = User
+    template_name = "usuario/registrar.html"
+    form_class = RegistroForm
+    success_url = reverse_lazy('mascota:mascota_listar')
+
